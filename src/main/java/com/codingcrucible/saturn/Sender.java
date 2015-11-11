80213fdc384d5065f0a58951f575c0627b7dd2c8
Luke Petrolekas <luke.petrolekas@gmail.com>
2015-11-10 19:26:04 -0500

    Remove unneeded import.

--- src/main/java/com/codingcrucible/saturn/Sender.java
+++ src/main/java/com/codingcrucible/saturn/Sender.java
@@ -1,14 +1,5 @@
 package com.codingcrucible.saturn;
 
-
-import com.codingcrucible.saturn.Message;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 public interface Sender {
     void send(Message m);
 }
